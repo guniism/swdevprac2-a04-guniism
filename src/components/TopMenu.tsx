687427de47883557a3f1eb55cb75135ca6fead89
guniism <gun16110@hotmail.com>
2025-03-13 05:34:12 +0700

    Modify style

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 export default function TopMenu(){
     return(
-        <div className="bg-white">
+        <div className="bg-white shadow-sm">
             <div className="max-w-6xl w-full mx-auto px-4">
-                <div className="h-14 flex flex-row-reverse shadow-sm">
+                <div className="h-14 flex flex-row-reverse ">
                     <div className="px-1 h-full flex items-center justify-center">
                         <Image src="/img/logo.png"
                             alt="Card Picture"
