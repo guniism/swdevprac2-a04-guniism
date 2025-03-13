38cd914f1e64f0a21f0788776037c79b8a8190a0
guniism <gun16110@hotmail.com>
2025-03-13 12:13:00 +0700

    Fix right menuItems

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function TopMenu(){
     return(
         <div className="bg-white shadow-sm">
-            <div className="max-w-6xl w-full mx-auto px-4">
+            {/* <div className="max-w-6xl w-full mx-auto px-4"> */}
                 <div className="h-14 flex flex-row-reverse ">
                     <div className="px-1 h-full flex items-center justify-center">
                         <Image src="/img/logo.png"
@@ -16,7 +16,7 @@ export default function TopMenu(){
                     </div>
                     <TopMenuItem menuName="Booking" link="/booking"/>
                 </div>
-            </div>
+            {/* </div> */}
         </div>
     )
 }
